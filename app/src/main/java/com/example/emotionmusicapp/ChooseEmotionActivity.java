012ac1b685d23bcd4d791c5079700c2d53c05458
PhiAn123 <48847287+PhiAn123@users.noreply.github.com>
2019-11-18 17:40:43 +0700

    First demo commit

---
+++ app/src/main/java/com/example/emotionmusicapp/ChooseEmotionActivity.java
@@ -0,0 +1,200 @@
+package com.example.emotionmusicapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.Config;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.AbsoluteLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.time.LocalDate;
+import java.util.Calendar;
+import java.util.Date;
+
+public class ChooseEmotionActivity extends AppCompatActivity {
+
+    ImageView mainBackGround, happyEarthEmotion, loveEarthEmotion, sadEarthEmotion, boringEarthEmotion, cryEarthEmotion, sickEarthEmotion, angryEarthEmotion;
+    AbsoluteLayout mainScreen;
+    LinearLayout questionTextLayout;
+    TextView happyEarthEmotionText, loveEarthEmotionText, sadEarthEmotionText, boringEarthEmotionText, cryEarthEmotionText, sickEarthEmotionText, angryEarthEmotionText;
+
+    TextView questionText;
+
+    Animation chooseEmotionScreenAni;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // code allow app to use full screen
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_choose_emotion);
+
+        castControl();
+        setChooseEmotionScreenBackground();
+        setControlAnimation();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        setChooseEmotionScreenBackground();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        setChooseEmotionScreenBackground();
+    }
+
+    @Override
+    public void finish() {
+        super.finish();
+
+        // add animation when user back to previous screen
+        Intent startMainActivity = new Intent(this, MainActivity.class);
+        startActivity(startMainActivity);
+        overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
+    }
+
+    // cast all icon and textview to set animation
+    public void castControl() {
+        mainBackGround = (ImageView) findViewById(R.id.chooseEmotionMainBackGround);
+        mainScreen = (AbsoluteLayout) findViewById(R.id.mainScreen);
+        questionTextLayout = (LinearLayout) findViewById(R.id.questionLayout);
+
+        happyEarthEmotion = (ImageView) findViewById(R.id.happyEarthEmotion);
+        happyEarthEmotionText = (TextView) findViewById(R.id.happyEmotionText);
+
+        loveEarthEmotion = (ImageView) findViewById(R.id.loveEarthEmotion);
+        loveEarthEmotionText = (TextView) findViewById(R.id.loveEmotionText);
+
+        sadEarthEmotion = (ImageView) findViewById(R.id.sadEarthEmotion);
+        sadEarthEmotionText = (TextView) findViewById(R.id.sadEmotionText);
+
+        boringEarthEmotion = (ImageView) findViewById(R.id.boringEarthEmotion);
+        boringEarthEmotionText = (TextView) findViewById(R.id.boringEmotionText);
+
+        cryEarthEmotion = (ImageView) findViewById(R.id.cryEarthEmotion);
+        cryEarthEmotionText = (TextView) findViewById(R.id.cryEmotionText);
+
+        sickEarthEmotion = (ImageView) findViewById(R.id.sickEarthEmotion);
+        sickEarthEmotionText = (TextView) findViewById(R.id.sickEmotionText);
+
+        angryEarthEmotion = (ImageView) findViewById(R.id.angryEarthEmotion);
+        angryEarthEmotionText = (TextView) findViewById(R.id.angryEmotionText);
+    }
+
+    // set animation for all emotion icon and textview
+    public void setControlAnimation() {
+        //chooseEmotionScreenAni = AnimationUtils.loadAnimation(ChooseEmotionActivity.this, R.anim.choose_emotion_screen_back_ground_ani);
+        //mainBackGround.startAnimation(chooseEmotionScreenAni);
+
+        chooseEmotionScreenAni = AnimationUtils.loadAnimation(ChooseEmotionActivity.this, R.anim.choose_emotion_screen_headertext_ani);
+        questionTextLayout.startAnimation(chooseEmotionScreenAni);
+
+        chooseEmotionScreenAni = AnimationUtils.loadAnimation(ChooseEmotionActivity.this, R.anim.happy_icon_and_text_ani);
+        happyEarthEmotion.startAnimation(chooseEmotionScreenAni);
+        chooseEmotionScreenAni = AnimationUtils.loadAnimation(ChooseEmotionActivity.this, R.anim.happy_icon_and_text_ani);
+        happyEarthEmotionText.startAnimation(chooseEmotionScreenAni);
+
+        chooseEmotionScreenAni = AnimationUtils.loadAnimation(ChooseEmotionActivity.this, R.anim.love_emotion_and_text_ani);
+        loveEarthEmotion.startAnimation(chooseEmotionScreenAni);
+        chooseEmotionScreenAni = AnimationUtils.loadAnimation(ChooseEmotionActivity.this, R.anim.love_emotion_and_text_ani);
+        loveEarthEmotionText.startAnimation(chooseEmotionScreenAni);
+
+        chooseEmotionScreenAni = AnimationUtils.loadAnimation(ChooseEmotionActivity.this, R.anim.sad_icon_and_text_ani);
+        sadEarthEmotion.startAnimation(chooseEmotionScreenAni);
+        chooseEmotionScreenAni = AnimationUtils.loadAnimation(ChooseEmotionActivity.this, R.anim.sad_icon_and_text_ani);
+        sadEarthEmotionText.startAnimation(chooseEmotionScreenAni);
+
+        chooseEmotionScreenAni = AnimationUtils.loadAnimation(ChooseEmotionActivity.this, R.anim.boring_icon_and_text_ani);
+        boringEarthEmotion.startAnimation(chooseEmotionScreenAni);
+        chooseEmotionScreenAni = AnimationUtils.loadAnimation(ChooseEmotionActivity.this, R.anim.boring_icon_and_text_ani);
+        boringEarthEmotionText.startAnimation(chooseEmotionScreenAni);
+
+        chooseEmotionScreenAni = AnimationUtils.loadAnimation(ChooseEmotionActivity.this, R.anim.cry_icon_and_text_ani);
+        cryEarthEmotion.startAnimation(chooseEmotionScreenAni);
+        chooseEmotionScreenAni = AnimationUtils.loadAnimation(ChooseEmotionActivity.this, R.anim.cry_icon_and_text_ani);
+        cryEarthEmotionText.startAnimation(chooseEmotionScreenAni);
+
+        chooseEmotionScreenAni = AnimationUtils.loadAnimation(ChooseEmotionActivity.this, R.anim.sick_icon_and_text_ani);
+        sickEarthEmotion.startAnimation(chooseEmotionScreenAni);
+        chooseEmotionScreenAni = AnimationUtils.loadAnimation(ChooseEmotionActivity.this, R.anim.sick_icon_and_text_ani);
+        sickEarthEmotionText.startAnimation(chooseEmotionScreenAni);
+
+        chooseEmotionScreenAni = AnimationUtils.loadAnimation(ChooseEmotionActivity.this, R.anim.angry_icon_and_text_ani);
+        angryEarthEmotion.startAnimation(chooseEmotionScreenAni);
+        chooseEmotionScreenAni = AnimationUtils.loadAnimation(ChooseEmotionActivity.this, R.anim.angry_icon_and_text_ani);
+        angryEarthEmotionText.startAnimation(chooseEmotionScreenAni);
+
+        setQuestionTextBackground();
+    }
+
+    public void setQuestionTextBackground() {
+        chooseEmotionScreenAni.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                // set background for question text after finish all animation
+                questionText = (TextView) findViewById(R.id.questionText);
+
+                Bitmap mainBackgroundBmp = ((BitmapDrawable) mainBackGround.getDrawable()).getBitmap();
+                Bitmap cropImg = Bitmap.createBitmap(mainBackgroundBmp, 0, 0, mainBackgroundBmp.getWidth(), mainBackgroundBmp.getHeight() - 940);
+
+                questionText.setBackground(new BitmapDrawable(getResources(), cropImg));
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+    }
+
+    public void setChooseEmotionScreenBackground() {
+        switch (Calendar.getInstance().get(Calendar.DAY_OF_WEEK)) {
+            case Calendar.MONDAY:
+                mainBackGround.setImageResource(R.drawable.music_notes_background);
+                break;
+            case Calendar.TUESDAY:
+                mainBackGround.setImageResource(R.drawable.music_word_background);
+                break;
+            case Calendar.WEDNESDAY:
+                mainBackGround.setImageResource(R.drawable.feeling_girl_background);
+                break;
+            case Calendar.THURSDAY:
+                mainBackGround.setImageResource(R.drawable.guitar_background);
+                break;
+            case Calendar.FRIDAY:
+                mainBackGround.setImageResource(R.drawable.headphone_background);
+                break;
+            case Calendar.SATURDAY:
+                mainBackGround.setImageResource(R.drawable.song_background);
+                break;
+            case Calendar.SUNDAY:
+                mainBackGround.setImageResource(R.drawable.music_background);
+                break;
+        }
+    }
+}
---
+++ app/src/main/java/com/example/emotionmusicapp/MainActivity.java
@@ -0,0 +1,172 @@
+package com.example.emotionmusicapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.AbsoluteLayout;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+
+    // appNameAndInstructionIcon, instructionIconLayout, startAppButtonLay
+
+    ImageView mainScreenBackground, cloverImg, aboutUsIcon, howToUseIcon;
+
+    LinearLayout appMainGreeting, appNameAndIcon, iconMenu, startAppButLay;
+    AbsoluteLayout mainScreen;
+
+    Button startButton;
+    TextView mainGreetingTV, aboutUsTV, howToUseTV, appName;
+
+    Animation appNameAndIconAni, startButtonAni;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_main);
+
+        castControl();
+    }
+
+    // check if application is running then update the greeting text
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        setMainGreetingText(); // set the main greeting (Good morning,...)
+    }
+
+    // check if application is resumed then update the greeting text
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        setMainGreetingText(); // set the main greeting (Good morning,...)
+    }
+
+    // cast all the control need to set animation
+    public void castControl() {
+        mainScreenBackground = (ImageView) findViewById(R.id.mainBackGround);
+        cloverImg = (ImageView) findViewById(R.id.clover);
+        aboutUsIcon = (ImageView) findViewById(R.id.aboutUsIcon);
+        howToUseIcon = (ImageView) findViewById(R.id.howToUseAppIcon);
+
+        appMainGreeting = (LinearLayout) findViewById(R.id.splashScreen);
+        appNameAndIcon = (LinearLayout) findViewById(R.id.appNameAndInstructionIcon);
+        iconMenu = (LinearLayout) findViewById(R.id.instructionIconLayout);
+        startAppButLay = (LinearLayout) findViewById(R.id.startAppButtonLay);
+
+        appNameAndIcon.setVisibility(View.INVISIBLE);
+        iconMenu.setVisibility(View.INVISIBLE);
+        startAppButLay.setVisibility(View.INVISIBLE);
+
+        mainScreen = (AbsoluteLayout) findViewById(R.id.mainScreen);
+
+        startButton = (Button) findViewById(R.id.startAppButton);
+
+        mainGreetingTV = (TextView) findViewById(R.id.greetingTV);
+        aboutUsTV = (TextView) findViewById(R.id.aboutUsTV);
+        howToUseTV = (TextView) findViewById(R.id.howToUseTV);
+        appName = (TextView) findViewById(R.id.appNameText);
+
+        setMainGreetingText();
+    }
+
+    // set the animation for each control
+    public void setControlAnimation() {
+        appNameAndIconAni = AnimationUtils.loadAnimation(this, R.anim.instruction_icon_ani);
+        appNameAndIcon.startAnimation(appNameAndIconAni);
+
+        appNameAndIconAni = AnimationUtils.loadAnimation(this, R.anim.icon_menu_ani);
+        iconMenu.startAnimation(appNameAndIconAni);
+
+        startButtonAni = AnimationUtils.loadAnimation(this, R.anim.start_button_ani);
+        startButton.startAnimation(startButtonAni);
+    }
+
+    // get the system time to set the greeting text
+    public void setMainGreetingText() {
+        Date currentTime = Calendar.getInstance().getTime();
+
+        if (currentTime.getHours() >= 0 && currentTime.getHours() < 12) {
+            mainGreetingTV.setText("Good Morning");
+        } else if (currentTime.getHours() >= 12 && currentTime.getHours() < 18) {
+            mainGreetingTV.setText("Good Afternoon");
+        } else if (currentTime.getHours() >= 18 && currentTime.getHours() <= 24) {
+            mainGreetingTV.setText("Good Evening");
+        }
+    }
+
+    // event for main screen touch to begin setting animation for all control
+    public void onMainScreenTouchListener(View view) {
+        mainScreenBackground.animate().translationY(-670).setDuration(800).setStartDelay(300);
+
+        //cloverImg.animate().alpha(0).setDuration(800).setStartDelay(600);
+        cloverImg.animate().translationX(-1000).setDuration(800).setStartDelay(600);
+
+        appMainGreeting.animate().translationY(140).alpha(0).setDuration(800).setStartDelay(300);
+
+        appNameAndIcon.setVisibility(View.VISIBLE);
+        iconMenu.setVisibility(View.VISIBLE);
+        startAppButLay.setVisibility(View.VISIBLE);
+
+        setControlAnimation();
+
+        mainScreen.setOnClickListener(null); // disable touch event after finish the animation
+    }
+
+    @Override
+    public void finish() {
+        super.finish();
+        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+    }
+
+    // event when user click the start app button
+    public void onStartButtonClickListener(View view) {
+        startButtonAni = AnimationUtils.loadAnimation(MainActivity.this, R.anim.start_button_gone_ani);
+        startButton.startAnimation(startButtonAni);
+
+        startButtonAni.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+                mainScreenBackground.animate().translationY(mainScreenBackground.getY() - 150).alpha(0).setDuration(200).setStartDelay(0);
+                aboutUsIcon.animate().translationX(-80).alpha(0).setDuration(200).setStartDelay(0);
+                howToUseIcon.animate().translationX(80).alpha(0).setDuration(200).setStartDelay(0);
+                aboutUsTV.animate().translationX(-80).alpha(0).setDuration(200).setStartDelay(0);
+                howToUseTV.animate().translationX(80).alpha(0).setDuration(200).setStartDelay(0);
+                appName.animate().alpha(0).setDuration(100).setStartDelay(0);
+                startButton.animate().translationY(100).alpha(0).setDuration(200).setStartDelay(0);
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                Intent startChooseEmotionScreen = new Intent(MainActivity.this, ChooseEmotionActivity.class);
+                startActivity(startChooseEmotionScreen);
+                finish();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+    }
+}
